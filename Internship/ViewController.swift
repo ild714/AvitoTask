ee689585e98873c28e7c28d37eaeb447dcb70feb
ildar714 <ildarnigmetzjanov@gmail.com>
2020-12-27 18:30:32 +0300

    Initial Commit

---
+++ Internship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Internship
+//
+//  Created by Ildar on 12/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
